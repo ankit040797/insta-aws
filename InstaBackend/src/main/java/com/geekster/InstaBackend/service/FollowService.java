9af61eaddde8a6a97605faddcd71a3664d28e5af
Ankit Singh <94648576+ankit040797@users.noreply.github.com>
2023-12-04 01:06:37 +0530

    Update FollowService.java

--- InstaBackend/src/main/java/com/geekster/InstaBackend/service/FollowService.java
+++ InstaBackend/src/main/java/com/geekster/InstaBackend/service/FollowService.java
@@ -11,10 +11,23 @@ import java.util.List;
 @Service
 public class FollowService {
 
-    @Autowired
+
+@Autowired
     IFollowRepo followRepo;
 
 
+    public void startFollowing(User follower, User target) {
+
+        Follow follow = new Follow(null,target,follower);
+        followRepo.save(follow);
+    }
+
+    public boolean findByTargetAndFollower(User follower, User target) {
+
+        List<Follow> follows =  followRepo.findByCurrentUserAndCurrentUserFollower(target,follower);
+
+        return !follows.isEmpty();
+    }
 
 
 }
